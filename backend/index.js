9f33505a9218c27348da82802991ca20838843ac
Shubham <shubhambiswas9899@gmail.com>
2025-02-13 21:31:33 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const app=express()
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));  
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}));
 
 mongoose.connect(process.env.MONGO)
   .then(() => {
